dd2d2405b116521a1f680069108f503edb1cbeef
Yunus Szönyi <62446345+yun-jay@users.noreply.github.com>
2022-10-09 19:55:53 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,45 +2,45 @@ module.exports = {
   root: true, // Make sure eslint picks up the config at the root of the directory
   parserOptions: {
     ecmaVersion: 2020, // Use the latest ecmascript standard
-    sourceType: "module", // Allows using import/export statements
+    sourceType: 'module', // Allows using import/export statements
     ecmaFeatures: {
-      jsx: true, // Enable JSX since we're using React
-    },
+      jsx: true // Enable JSX since we're using React
+    }
   },
   settings: {
     react: {
-      version: "detect", // Automatically detect the react version
-    },
+      version: 'detect' // Automatically detect the react version
+    }
   },
   env: {
     es6: true,
     browser: true, // Enables browser globals like window and document
     amd: true, // Enables require() and define() as global variables as per the amd spec.
-    node: true, // Enables Node.js global variables and Node.js scoping.
+    node: true // Enables Node.js global variables and Node.js scoping.
   },
   extends: [
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react-prefer-function-component/recommended",
-    "plugin:prettier/recommended", // Make this the last element so prettier config overrides other formatting rules
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-prefer-function-component/recommended',
+    'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
-    "no-console": "error",
-    "prefer-arrow-functions/prefer-arrow-functions": "error",
-    "react/prop-types": "off",
-    "react/no-unknown-property": ["error", { ignore: ["jsx", "global"] }],
-    "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Use our .prettierrc file as source
-    "react/react-in-jsx-scope": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "error",
+    'no-console': 'error',
+    'prefer-arrow-functions/prefer-arrow-functions': 'error',
+    'react/prop-types': 'off',
+    'react/no-unknown-property': ['error', { ignore: ['jsx', 'global'] }],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
       {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"],
-      },
-    ],
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton']
+      }
+    ]
   },
-  plugins: ["react-prefer-function-component", "prefer-arrow-functions"],
+  plugins: ['react-prefer-function-component', 'prefer-arrow-functions']
 };
